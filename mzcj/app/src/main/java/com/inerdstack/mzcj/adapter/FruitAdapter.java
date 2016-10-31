d9b945afb7dd0d718c987ae7c5f4bcea36b30831
iNerdStack <wjnovember@qq.com>
2016-10-31 14:33:18 +0800

    update

---
+++ mzcj/app/src/main/java/com/inerdstack/mzcj/adapter/FruitAdapter.java
@@ -0,0 +1,25 @@
+package com.inerdstack.mzcj.adapter;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+
+/**
+ * 项目名称：mzcj
+ * 模块名称：
+ * 包名：com.inerdstack.mzcj.adapter
+ * 类功能：
+ * 创建人：inerdstack
+ * 创建时间：2016 16/10/23 下午2:59
+ * 联系邮箱: wjnovember@icloud.com
+ */
+public class FruitAdapter extends ArrayAdapter {
+
+    public FruitAdapter(Context context, int resource, int textViewResourceId) {
+        super(context, resource, textViewResourceId);
+
+    }
+
+
+}
